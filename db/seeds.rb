850c55eaf59535adb55ee350e1a5db7ffc5de04f
jorge gonzalez <gonzalezjor13@gmail.com>
2025-03-26 16:04:33 -0500

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+CATEGORIES = %w(chinese japanese italian french belgium)
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: CATEGORIES.sample,
+  )
+end
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
